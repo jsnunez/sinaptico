780fb3e71ff7084147169d7cb31b9c6ad64d3dcb
joan sebastian nuñez serrano <jsnunez94@gmail.com>
2025-04-03 15:12:02 -0500

    incio proyecto

---
+++ app/public/admin.js
@@ -0,0 +1,31 @@
+
+document.getElementById("cerrarSesion").addEventListener("click",()=>{
+  document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.location.href = "/"
+})
+document.getElementById("estado").addEventListener("click",()=>{
+  console.log("hola");
+var entidades= document.getElementById("entidades");
+entidades.style.display="none";
+})
+
+// Función para obtener el valor de una cookie por su nombre
+function obtenerCookie(nombre) {
+  const nombreCookie = `${nombre}=`;
+  const cookies = document.cookie.split(';');
+  
+  for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim();
+      
+      if (cookie.indexOf(nombreCookie) === 0) {
+          return cookie.substring(nombreCookie.length, cookie.length);
+      }
+  }
+  
+  return null; // Retorna null si no se encuentra la cookie
+}
+
+// Ejemplo de cómo leer la cookie "usuario"
+const usuario = obtenerCookie("user");
+
+document.getElementById('bienvenido').innerText = "Bienvenido  " + usuario;
